3d3f166f64093d23ce452e47d65170d10f7fb6b4
Alexey Volkov <alexey.volkov@shopify.com>
2026-03-19 02:27:48 -0700

    Normalized the URL slashes

--- packages/app/src/entry.tsx
+++ packages/app/src/entry.tsx
@@ -98,9 +98,10 @@ if (!(root instanceof HTMLElement) && import.meta.env.DEV) {
 }
 
 const getCurrentUrl = () => {
+  let serverBaseUrl = import.meta.env.VITE_OPENCODE_SERVER_BASE_URL ?? ""
+  // Normalizing slashes. Add starting slash if needed, remove trailing slash. Default value will be empty string.
+  serverBaseUrl = ("/" + serverBaseUrl.replace(/^\//, "")).replace(/\/$/, "")
   if (location.hostname.includes("opencode.ai")) return "http://localhost:4096"
-  const serverBaseUrl =
-    "/" + (import.meta.env.VITE_OPENCODE_SERVER_BASE_URL ?? "/").replace(/^\//, "").replace(/\/$/, "")
   if (import.meta.env.DEV)
     return `http://${import.meta.env.VITE_OPENCODE_SERVER_HOST ?? "localhost"}:${import.meta.env.VITE_OPENCODE_SERVER_PORT ?? "4096"}{serverBaseUrl}`
   return location.origin + serverBaseUrl
